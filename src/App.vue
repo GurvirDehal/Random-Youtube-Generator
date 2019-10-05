d6f7e1581fdb070629a0afa73dc8319741562061
Gurvir <gurvirdehal@gmail.com>
2019-10-05 13:09:55 -0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Title"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
