1a882338d03d9e7e788eb52182c360fb2d0cc864
GurvirDehal <gurvirdehal@gmail.com>
2019-10-26 16:22:58 -0600

    Add YouTube API

---
+++ src/components/YouTubeMedia.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+    iframe(id="ytplayer"
+           width="560" 
+           height="315" 
+           :src="videoUrl"
+           frameborder="0" 
+           allow="autoplay; encrypted-media"
+           allowfullscreen)
+</template>
+
+<script>
+  // var tag = document.createElement('script');
+  // tag.src = "//www.youtube.com/iframe_api";
+  // var firstScriptTag = document.getElementsByTagName('script')[0];
+  // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  // var player
+
+  // function onYouTubeIframeAPIReady() {
+  //     player = new YT.Player('ytplayer_1-5', {events: {'onReady': onPlayerReady}});
+  // }
+
+  // function onPlayerReady(event) {
+  //     event.target.playVideo();
+  // }
+
+export default {
+    props: {
+      videoId: {
+        type: String,
+        default: ''
+      }
+    },
+    computed: {
+      videoUrl: function() {
+        return `https://www.youtube.com/embed/${this.videoId}?autoplay=1`
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/YouTubePicker.vue
+++ src/components/YouTubePicker.vue
@@ -1,31 +1,50 @@
-<template>
-    <div id = "YoutubeGen">
-        <h1> Random YouTube Generator </h1>
-        <button @click="getRandomWord"> Text </button>
-        <p> {{ word }} </p>
+<template lang="pug">
+    #YouTubeGen
+        h1 Random YouTube Generator
+        button(@click="getRandomWord") Click Me
+        you-tube-media(v-if="videoId" :videoId="videoId")
     </div>
 </template>
 
 <script>
+import YouTubeMedia from './YouTubeMedia'
 export default {
+  components: {
+      YouTubeMedia
+  },
   props: {
-      lol: Function
+
   },
   data: function() {
       return {
-          word: ''
+          word: '',
+          videoId: '',
       }
   },
   methods: {
       async getRandomWord() {
-          const response = await fetch('https://random-word-api.herokuapp.com/word?key=6RS9GK59&number=1')
+          const response = await fetch(`http://api.wordnik.com/v4/words.json/randomWord?api_key=${process.env.VUE_APP_WORDNIK}`)
           const myJson = await response.json()
-          this.word = myJson[0]
+          this.word = myJson.word
+          this.getVideo(this.word)
+      },
+      async getVideo(query) {
+          const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${process.env.VUE_APP_API_KEY}`)
+          const myJson = await response.json()
+          this.videoId = myJson.items[0].id.videoId
       }
   }
 }
 </script>
 
 <style scoped>
+#YouTubeGen {
+    display: flex;
+    flex-direction: column;
+}
 
+button {
+    width: fit-content;
+    margin-bottom: 20px;
+}
 </style>
